b9dad3d9bc8d33a39b26a9f526f77335de3fc76a
JelsonJr <jelson.rodrigues.junior@gmail.com>
2024-04-25 14:28:45 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/oficina/oficinaadmin/OficinaAdminApplication.kt
@@ -0,0 +1,11 @@
+package br.com.oficina.oficinaadmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OficinaAdminApplication
+
+fun main(args: Array<String>) {
+	runApplication<OficinaAdminApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/oficina/oficinaadmin/OficinaAdminApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.oficina.oficinaadmin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OficinaAdminApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
